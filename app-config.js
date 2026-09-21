4422a0a6785128ed5e6e56ba47f90c8e18682425
turia-juggerclub <turiajuggerclub@gmail.com>
2026-09-21 12:09:46 +0200

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,3 +1,3 @@
 // URL publica de la aplicacion de Google Apps Script.
 // Si algun dia creas una implementacion NUEVA con otra URL, cambia solo esta linea.
-window.TURIA_APP_URL = 'https://script.google.com/macros/s/AKfycbygDqKxp7hPYTArehYLnla1Aqx-TP3SFItbJVG0-ckEUE6VsQ1K4NpdiSnoHtdM3vIJIw/exec';
+window.TURIA_APP_URL = 'https://script.google.com/macros/s/AKfycbwWV8Cz3HA_Fu3GcPArZk5QM6i9vKPe7CzRTzvATyt_9zRm0K37lu5rvj2KfS9rb7NY_Q/exec';
